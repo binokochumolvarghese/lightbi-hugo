e96e729363fcef63a306595d8fc75bde7285ee0d
Wladimir Palant <fqcgithub@palant.de>
2023-12-29 01:15:09 +0100

    Improve dark mode handling

--- static/js/dark-mode.js
+++ static/js/dark-mode.js
@@ -1,47 +1,43 @@
 // from radu-matei.com
 
-var toggle = document.getElementById("dark-mode-toggle");
-var darkTheme = document.getElementById("dark-mode-theme");
-
-var systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
-  ? "dark"
-  : "light";
-
-// the default theme is light
-var savedTheme = localStorage.getItem("dark-mode-storage") || systemTheme;
-setTheme(savedTheme);
+updateTheme();
 
 // set the appropriate theme when the user toggles the button
-toggle.addEventListener("click", () => {
-    if (toggle.className === "bi bi-moon-stars") {
-        setTheme("dark");
-        localStorage.setItem("dark-mode-storage", "dark");
-    } else if (toggle.className === "bi bi-sun") {
-        setTheme("light");
-        localStorage.setItem("dark-mode-storage", "light");
-    }
-});
+window.addEventListener("DOMContentLoaded", () => {
+  var toggle = document.getElementById("dark-mode-toggle");
+  toggle.addEventListener("click", () => {
+    if (document.documentElement.getAttribute("data-theme") == "dark")
+      localStorage["dark-mode-storage"] = "light";
+    else
+      localStorage["dark-mode-storage"] = "dark";
+    updateTheme();
+  });
+}, {once: true});
 
 // Add an event listener when the browser theme changes.
 // The user defined theme does take precedence, so if the
-// changed the browser theme, do not persist the change
-// across refreshes.
+// changed the browser theme, forget previous preference.
 window
   .matchMedia("(prefers-color-scheme: dark)")
-  .addEventListener("change", (event) => {
-    if (event.matches) {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
+  .addEventListener("change", () => {
+    delete localStorage["dark-mode-storage"];
+    updateTheme();
   });
 
+// React to theme changes in other tabs
+window.addEventListener("storage", event => {
+  if (event.key == "dark-mode-storage")
+    updateTheme();
+});
+
+function updateTheme() {
+  let userPreference = localStorage["dark-mode-storage"];
+  if (userPreference == "dark" || userPreference == "light")
+    setTheme(userPreference);
+  else
+    setTheme(window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+}
+
 function setTheme(mode) {
-    if (mode === "dark") {
-        darkTheme.disabled = false;
-        toggle.className = "bi bi-sun";  
-    } else if (mode === "light") {
-        darkTheme.disabled = true;
-        toggle.className = "bi bi-moon-stars"; 
-    }
-} 
+  document.documentElement.setAttribute("data-theme", mode);
+}
